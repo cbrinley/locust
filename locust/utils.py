2c75f6dc9b69e6c6bc4232deaf7e283fd0f83dbb
chris brinley <chris.brinley@morningstar.com>
2013-01-31 13:19:55 -0600

    Condensed decorator code and updated comments

--- locust/utils.py
+++ locust/utils.py
@@ -2,79 +2,84 @@
 utility functions to be of general use.
 '''
 
-class Decorators(object):
-		@staticmethod
-		def decorator(fn):
-			'''takes a standalone function and makes it a decorator.
-				 used to keep you code looking cleaner. decorators can 
-				 be kinda ugly to look at.
-				 :param fn: function to turn into a decorator. function must\
-				 accept the target of decoration as its first argument.
-				 Usage pattern is as follows:
+def decorator(fn):
+  '''this function will convert a vanilla python
+     function into a decorator. This allows for
+     much cleaner decorator definitions and 
+     easy extension to support optional parameters
+     without having to update all usage sites.
+     the generated decorator will have the following
+     properties:
+      * it may used to decorate target functions
+        with or with out optional arguments. See
+        below for example.
+      * the decorated callable is passed as the
+        first argument to the vanilla function.
+      * any optional parameters specified  in the
+        decorator use are passed as a the second
+        argument to the vanilla function. The type
+        of this argument is dictionary.
+      * any arguments to the decorated function are
+        passed as the remaining arguments to the 
+        vanilla function and can be captured with
+        standard *args,**kwargs syntax.
+      
+      === USAGE ===
+      
+      from utils import decorator
+      
+      @decorator
+      def log_call(target,options,*args,**kwargs):
+        "I log all calls to decorated functions."
+        level = options.get("level","INFO")
+        fname = target.__name__
+        log_function_call(fname,level,phase="entry",*args,**kwargs)
+        ret = target(*args,**kwargs)
+        log_function_call(fname,level,phase="exit")
+        return ret
 
-				 from utils import Decorators
-				 
-				 @Decorators.decorator
-				 def my_logging_decorator(decoratored,*args,**kwargs):
-				   log.info("calling %s" % decorated)
-				   return decorated(*args,**kwargs)
+      @log_call(level="DEBUG")
+      def latent_call(arg1,arg2):
+        #do latent stuff
 
-				 @my_logging_decorator
-				 def do_work(worker):
-				   return worker.work()
-			'''
-			def decorator_wrapper(decorated_function):
-				def stand_in_decorator(*args,**kwargs):
-					'''This is the function that will actually be the 
-					   decorator we see if we were to inspect the target.
-					   From our example:
-					   print do_work 
-					   <function stand_in_decorator at 0x023AB8B0>
-					'''
-					return fn(decorated_function,*args,**kwargs)
-				return stand_in_decorator
-			return decorator_wrapper
+      @log_call
+      def highly_latent_call(arg3,optional_arg=None)
+        #do highly latent stuff
 
+  '''
+  '''=== Code Comments ===
+     #1 This returns a function which may accept either a callable
+        as is the standard case for decorators or a set of options
+        which is the case for some decorators.
 
-		@staticmethod
-		def instance_decorator(fn):
-			'''syntax sugar. it clearly marks the
-			   decorator as only useful on a instance method
-			   as it will require a reference to "self"
-			'''
-			return Decorators.decorator(fn)
+     #2 if kwargs was found we now know we are evaluating usage of 
+        a decorator with options provided.
+     #3 since we have now consumed the optional arguments we must 
+        now return a function that can consume a callable. aka 
+        the standard decorator pattern.
+     #4 when inspecting the code this will be the function that is
+        observed as the "bound method":
+  Example from python shell:
+  >>> myobj = MyObject() #where MyObject.bar has had decorator applied.
+  >>> myobj.bar
+  <bound method MyObject.stand_in_decorator of <__main__.MyObject object at 0x02566BF0>>
+     #5 We see that no options were passed. Proceed with standard
+        decorator pattern. See line item #4
+     #6 Of note, decorated_function is a tuple due to the *decorated_function
+        syntax used in decorator_wrapper definition. Pull off the 
+        first element of this tuple and pass this as vanilla function
+        as though it was a traditional decorator.
+  '''
+  def decorator_wrapper(*decorated_function,**decorator_kwargs): #1
+    if decorator_kwargs: #2
+      def accepts_callable(decorated_function): #3
+        def stand_in_decorator(*args,**kwargs): #4
+          return fn(decorated_function,decorator_kwargs,*args,**kwargs)
+        return stand_in_decorator
+      return accepts_callable
+    else: #5
+      def stand_in_decorator(*args,**kwargs): #4
+        return fn(decorated_function[0],decorator_kwargs,*args,**kwargs) #6
+      return stand_in_decorator
+  return decorator_wrapper
 
-		@staticmethod
-		def options_decorator(fn):
-			'''this decorator will allow your function to
-			   be called with or without keyword arguments:
-			   NOTE: positional arguments are not supported.
-			   @optional_args_decorator
-			   def mydec(decorated,options,*args,**kwargs):
-			     decorator_keyword_arg = options.get('myKeyword',"kw_default")
-			     #do conditional stuff
-			     return decorated(*args,**kwargs)
-
-			   @mydec
-			   def foo():
-
-			   and 
-
-			   @mydec(some_kw_args=1234)
-			   def foo():
-
-			   will work without the ugyly setup code. you
-			   must still check if your arguments are None type.
-			'''
-			def decorator_wrapper(*decorated_function,**decorator_kwargs):
-				if decorator_kwargs:
-					def accepts_callable(decorated_function):
-						def stand_in_decorator(*args,**kwargs):
-							return fn(decorated_function,decorator_kwargs,*args,**kwargs)
-						return stand_in_decorator
-					return accepts_callable
-				else:
-					def stand_in_decorator(*args,**kwargs):
-						return fn(decorated_function[0],decorator_kwargs,*args,**kwargs)
-					return stand_in_decorator
-			return decorator_wrapper
